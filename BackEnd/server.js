c011e03b9396b1e9ad383c4f871d82259c94934b
Abhijit Das <sadabhijit2004@gmail.com>
2025-03-24 20:13:13 +0530

    Modifying PORT

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const app = require('./src/app')
 
 
-
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
